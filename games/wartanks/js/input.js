bfdd1c704e77f966fedaa199b811dede34dbe54c
erichagemeyer <erichagemeyer@gmail.com>
2015-01-24 16:03:02 -0500

    control panel styling

--- games/wartanks/js/input.js
+++ games/wartanks/js/input.js
@@ -47,32 +47,32 @@ var Input = {
 		//	alert(e.keyCode);
 			switch(e.keyCode){
 		
-			case 32:
+			case 32: // Spacebar
 				e.preventDefault();
 				Tanks.fire();
 			break;
 		
-			case 37:
+			case 37: // Left Arrow
 				e.preventDefault();
 				var tank = Tanks.units[Game.player];
 				tank.x--;
 				tank.grounded = false;
 				$("#tank-pos").val(tank.x);
 			break;
-				case 39:
+				case 39: // Right Arrow
 					e.preventDefault();
 					var tank = Tanks.units[Game.player];
 					tank.x++;
 					tank.grounded = false;
 					$("#tank-pos").val(tank.x);
 				break;
-				case 38:
+				case 38: // Up Arrow
 					e.preventDefault();
 					var tank = Tanks.units[Game.player];
 					$('#tank-ang').val(parseInt($('#tank-ang').val()) - 5);
 					tank.turret = $('#tank-ang').val() / 180 * Math.PI;
 				break;
-				case 40:
+				case 40: // Down Arrow
 					e.preventDefault();
 					var tank = Tanks.units[Game.player];
 					$('#tank-ang').val(parseInt($('#tank-ang').val()) + 5);
@@ -95,8 +95,8 @@ var Input = {
 		});
 		
 		$(document).mousemove(function(e){
-			Input.x = e.pageX * 1000 / $(window).width();		
-			Input.y = e.pageY * 600 / $(window).height();
+			Input.x = e.pageX * 1000 / $(".game-canvas").width();		
+			Input.y = e.pageY * 600 / $(".game-canvas").height();
 
 			var tank = Tanks.units[Game.player];
 
