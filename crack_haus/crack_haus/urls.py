1ac444e5da7c06260541e6cdcecd3c8a41617f3a
Forrest Chromebook <forrestp@nvbots.com>
2015-03-14 19:13:07 -0400

    moved api, added index

--- crack_haus/crack_haus/urls.py
+++ crack_haus/crack_haus/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from games.views import GameViewSet, PlayerViewSet
 from rest_framework import routers
-
+from django.views.generic import TemplateView
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
@@ -11,9 +11,10 @@ router.register(r'games', GameViewSet)
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'crack_haus.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='games/index.html'), name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^', include(router.get_urls())),
+
+    url(r'^api/', include(router.get_urls())),
     # url(r'^', include('games.urls')),
 
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
